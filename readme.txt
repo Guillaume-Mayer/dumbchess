d0f76145f7702be405a1f4427f29117e3dbbac80
Guillaume Mayer <gmayer@arkho.tech>
2017-04-26 12:46:26 -0300

    Chess playing :)

--- readme.txt
+++
@@ -1,8 +0,0 @@
-I am learning Node.js, NPM and Git.
-
-This module does nothing for now.
-
-I want to make it the server for an online chess game.
-It may be RESTful (but I'm not sure at this point).
-
-The first step will be to allow two players to play together.
--- server.js
+++ server.js
@@ -1,27 +1,78 @@
-// Modules needed
-var express = require("express");
+// Modules
+var express = require('express');
+var chess = require('./dumb-chess');
 
-// Routes for dumbchess API
-var router = express.Router();
+// Get legal moves in algebraic notation and sorted
+function getLegalMoves() {
+	return chess.getLegalMoves()
+		.map(
+			function(m) {
+				return chess.moveToStr(m);
+			})
+		.sort(
+			function(a, b) {
+				return a.localeCompare(b);
+			});
+}
 
-router.get("/", function(req, res) {
-	res.json({ msg: "Welcome to my REST api"});
-});
+// Restart the game by unplaying all history moves
+function restart() {
+   while(chess.history.length > 0) {
+        chess.unplay(chess.history[chess.history.length - 1]);
+    }
+}
 
-router.get("/:game/join", function(req, res) {
-	res.json({ msg: "Rejoindre la partie " + req.params.game});	
-});
+// Unplay the last move
+function unplay() {
+	// Check if there is one
+	if (chess.history.length === 0) return;
+	// Unplay the last move
+	chess.unplay(chess.history[chess.history.length - 1]);
+}
+
+// Create the common response
+function resp(res) {
+	res.json({
+		colorToPlay: chess.colorToPlay() == chess.BLACK ? 'Black' : 'White',
+		fen: chess.posToFen(),
+		legalMoves: getLegalMoves().map(function(m) {return '/chess/play/'+ m;}),
+		actions: chess.history.length > 0 ? ['/chess/restart', '/chess/unplay'] : undefined,
+		history: chess.history.map(function(m) {return chess.moveToStr(m);})
+	});
+}
 
 // Express app
 var app = express();
 
-// Runs in /chess
-app.use("/chess", router);
-app.use(function(req, res) {
-	res.status(404).end();
-})
+// Routes
+app.get('/chess', function(req, res) {
+	resp(res);
+});
+app.get('/chess/play/:move', function(req, res) {
+	var moves = chess.getLegalMoves();
+	var move;
+	for (var m = 0; m < moves.length; m++) {
+		if (chess.moveToStr(moves[m]) == req.params.move) {
+			move = moves[m];
+		}
+	}
+	if (move === undefined) {
+		res.json({msg: 'Move [' + req.params.move + '] is not legal'});
+	} else {
+		chess.play(move);
+		resp(res);
+	}
+});
+app.get('/chess/restart', function(req, res) {
+	restart();
+	resp(res);
+});
+app.get('/chess/unplay', function (req, res) {
+	unplay();
+	resp(res);
+});
 
 // Start the server
 var port = 8080;
 app.listen(port);
-console.log("Server running at port " + port);
+console.log('Server running at port ' + port);
